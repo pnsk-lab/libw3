c85e7bc998f88fd115409e556020d8b71ec28c20
Nishi <nishi@nishi.boats>
2024-01-22 02:58:39 +0000

    fixed fetch
    git-svn-id: file:///raid/svn-main/nishi-libw3/trunk@47 d27a3e52-49c5-7645-884c-6793ebffc270

--- Example/fetch.c
+++ Example/fetch.c
@@ -36,7 +36,7 @@ int main(int argc, char** argv){
 		return 1;
 	}
 	W3_Library_Init();
-	struct W3* w3 = W3_Create("file", argv[1], 80);
+	struct W3* w3 = W3_Create("http", argv[1], 80);
 	if(w3 != NULL){
 		W3_Set_Method(w3, argv[3] == NULL ? "GET" : argv[3]);
 		W3_Set_Path(w3, argv[2]);
