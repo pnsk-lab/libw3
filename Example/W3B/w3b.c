cac9c6255a882820bc73900d625e947dc001a119
Nishi <nishi@nishi.boats>
2024-01-26 02:09:55 +0000

    fixing makefile, and version flag
    git-svn-id: file:///raid/svn-main/nishi-libw3/trunk@73 d27a3e52-49c5-7645-884c-6793ebffc270

--- Example/W3B/w3b.c
+++ Example/W3B/w3b.c
@@ -4,8 +4,17 @@
  * Simple browser
  */
 
+#include <W3Core.h>
+
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char** argv) {
-	
+	int i;
+	for(i = 1; i < argc; i++){
+		if(strcmp(argv[i], "--version") == 0){
+			printf("LibW3 %s\n", LIBW3_VERSION);
+			return 0;
+		}
+	}
 }
