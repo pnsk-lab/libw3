f67eef5e0696e4ce166a95370b217bee99c39acd
Nishi <nishi@nishi.boats>
2024-01-26 23:21:33 +0000

    gopher headers
    git-svn-id: file:///raid/svn-main/nishi-libw3/trunk@97 d27a3e52-49c5-7645-884c-6793ebffc270

---
+++ Library/Gopher.c
@@ -0,0 +1,5 @@
+/* $Id$ */
+#include "W3Gopher.h"
+
+void __W3_Gopher_Request(struct W3* w3) {
+}
---
+++ Library/W3Gopher.h
@@ -0,0 +1,17 @@
+/* $Id$ */
+#ifndef __W3GOPHER_H__
+#define __W3GOPHER_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "W3Core.h"
+
+void __W3_Gopher_Request(struct W3* w3);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
