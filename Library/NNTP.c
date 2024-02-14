37e18311cb108534582445024596aabccc3cbb12
Nishi <nishi@nishi.boats>
2024-02-14 05:34:53 +0000

    forgot to add
    git-svn-id: file:///raid/svn-main/nishi-libw3/trunk@216 d27a3e52-49c5-7645-884c-6793ebffc270

---
+++ Library/NNTP.c
@@ -0,0 +1,13 @@
+/* $Id$ */
+#include "W3NNTP.h"
+
+#include "W3Core.h"
+#include "W3DNS.h"
+#include "W3Util.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+extern int strcasecmp(const char* s1, const char* s2);
+
+void __W3_NNTP_Request(struct W3* w3) { __W3_Debug("LibW3-NNTP", "Sending the request"); }
--- W3Version.h.p
+++ W3Version.h.p
@@ -6,7 +6,7 @@
 extern "C" {
 #endif
 
-#define LIBW3_VERSION "2.12" \
+#define LIBW3_VERSION "2.12A" \
 SUFFIX
 
 #ifdef __cplusplus
